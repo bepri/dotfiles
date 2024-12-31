45494a588ef52934b74fb8478bbfb386558347a5
Imani Pelton <imani@bepri.dev>
2024-12-31 16:36:17 -0500

    feat: slightly modernize 1password ssh sync and make it optional

--- run_op_ssh.py
+++ run_op_ssh.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 # Masterfully formulated by Sir Dr. Rev. Peloton, Esq.
 
-import json # Loads the output of 1Password's CLI
-import sys # Used for output redirection (sys.stderr) and exits
-import shutil # For several convenience functions, notably a UNIX `which`-like function and gzipping
+import json  # Loads the output of 1Password's CLI
+import sys  # Used for output redirection (sys.stderr) and exits
+import shutil  # For several convenience functions, notably a UNIX `which`-like function and gzipping
 import os
 import re
 
 # Set if this program seems to be selecting the wrong executable for op
 OP_EXE = None
 
+
 def is_wsl() -> bool:
-    """Returns whether or not this script is being executed in a WSL environment.
-    """
+    """Returns whether or not this script is being executed in a WSL environment."""
     import platform
-    return re.search("microsoft", platform.uname().release, re.IGNORECASE) != None
+
+    return re.search("microsoft", platform.uname().release, re.IGNORECASE) is not None
+
 
 def get_op_exe() -> str | None:
     """Find the appropriate executable for the 1Password CLI
@@ -24,11 +26,12 @@ def get_op_exe() -> str | None:
     """
     if OP_EXE:
         return OP_EXE
-        
+
     if is_wsl():
         return shutil.which("op.exe")
     else:
         return shutil.which("op")
+
 
 def call_shell(cmd: str) -> str:
     """Calls shell within child process and returns stdout
@@ -37,18 +40,20 @@ def call_shell(cmd: str) -> str:
     Returns:
         str: Stdout of `cmd`
     """
-    import shlex # For splitting
-    import subprocess # Duh
-    res = b''
+    import shlex  # For splitting
+    import subprocess  # Duh
+
+    res = b""
     with subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE) as p:
         for line in p.stdout:
             res += line
 
-    return str(res, 'ascii')
+    return str(res, "ascii")
+
 
 class SSHConfigEntry:
-    """Wrapper for SSH config entries. Stores basic config parameters.
-    """
+    """Wrapper for SSH config entries. Stores basic config parameters."""
+
     def __init__(self, json: list[dict]):
         """Creates an entry from the output of `json.loads()`, which in turn should be loading the JSON-formatted return of an SSH key from the 1Password CLI.
 
@@ -60,15 +65,17 @@ class SSHConfigEntry:
             ValueError: Raised if `url` is not specified in `chezmoi params`.
         """
         # 1Password CLI always returns fields in the order specified by the command, so this can be hardcoded
-        self.key = json[0]['value']
+        self.key = json[0]["value"]
 
         # Extract the key's title in 1Password from the secret reference
         # This is a really janky workaround to avoid polling 1Password separately for the fields and the title
         # Retrieving the entire password entry isn't so much of an option either as the JSON output's format is (somehow) not all that machine friendly for parsing
         # So anyways, here we are
-        ref = json[0]['reference']
+        ref = json[0]["reference"]
         try:
-            self.keyname = re.match(r'^op:\/\/(?:\w+\s*)*?\/((?:\w+\s*)*)/.*$', ref).group(1)
+            self.keyname = re.match(
+                r"^op:\/\/(?:\w+\s*)*?\/((?:\w+\s*)*)/.*$", ref
+            ).group(1)
         except IndexError:
             self.keyname = None
 
@@ -79,21 +86,23 @@ class SSHConfigEntry:
         self.url: None | str = None
 
         # Loop through the provided parameters and set any that we find
-        parameters: list[str] = json[1]['value'].split('\n')
+        parameters: list[str] = json[1]["value"].split("\n")
         for param in parameters:
-            key, value = param.split(' ', 1)
+            key, value = param.split(" ", 1)
             setattr(self, key, value)
 
         # URL is the only required parameter, as we don't even know what this key connects to otherwise
-        if self.url == None:
-            raise ValueError(f'Key does not specify a URL to connect to! Add a URL field to the 1Password entry for {self.keyname}')
-        
+        if self.url is None:
+            raise ValueError(
+                f"Key does not specify a URL to connect to! Add a URL field to the 1Password entry for {self.keyname}"
+            )
+
         # Fall back if we can't parse the key name from the secret reference
-        if self.keyname == None:
+        if self.keyname is None:
             self.keyname = self.url
 
         # Make the resulting key name a little more friendly when serialization time comes
-        self.keyname = self.keyname.replace(' ', '_')
+        self.keyname = self.keyname.replace(" ", "_")
 
     def _make_key(self, path: str):
         """Generate the key file
@@ -101,11 +110,11 @@ class SSHConfigEntry:
         Args:
             path (str): Path to output the key file
         """
-        fullpath = os.path.join(path, self.keyname + '.pub')
-        with open(fullpath, 'w') as fout:
+        fullpath = os.path.join(path, self.keyname + ".pub")
+        with open(fullpath, "w") as fout:
             fout.write(self.key)
         os.chmod(fullpath, 0o600)
-            
+
     def serialize(self, path: str) -> str:
         """Represent this class instance as a valid SSH config entry
 
@@ -115,44 +124,45 @@ class SSHConfigEntry:
         Returns:
             str: Serialized form of the class instance
         """
-        print(f'Generating key and config entry for {self.keyname}')
+        print(f"Generating key and config entry for {self.keyname}")
 
         # Generate the key file
         self._make_key(path)
 
         # Output the required pieces of the config entry
         res = f'Host {self.url} {self.aliases.replace(",", " ") if self.aliases else ""}\n'
-        res += f'\tHostName {self.url}\n'
+        res += f"\tHostName {self.url}\n"
         res += f'\tIdentityFile "{os.path.join('%d', '.ssh', self.keyname + '.pub')}"\n'
-        res += f'\tIdentitiesOnly yes\n'
-        
+        res += "\tIdentitiesOnly yes\n"
+
         if self.user:
-            res += f'\tUser {self.user}\n'
+            res += f"\tUser {self.user}\n"
 
         # Set all of the extra options exactly as specified in the 1Password entry
         if self.options:
-            opts = self.options.split(',')
+            opts = self.options.split(",")
             for opt in opts:
-                res += f'\t{opt}\n'
+                res += f"\t{opt}\n"
 
         return res
-    
+
+
 def get_ssh_path() -> str:
-    """Get the path to the `.ssh` directory on the local machine. Only supports UNIX-like environments
-    """
+    """Get the path to the `.ssh` directory on the local machine. Only supports UNIX-like environments"""
     if is_wsl():
         # Use PowerShell to derive the Windows username if possible
-        if ps := shutil.which('powershell.exe'):
-            win_user = call_shell(f'{ps} \'$env:UserName\'').strip()
+        if ps := shutil.which("powershell.exe"):
+            win_user = call_shell(f"{ps} '$env:UserName'").strip()
         else:
-            print('Hi! You appear to be using WSL, so we need some extra info.')
-            win_user = input('What is your Windows username? ')
-            print('Thanks, back to work!')
+            print("Hi! You appear to be using WSL, so we need some extra info.")
+            win_user = input("What is your Windows username? ")
+            print("Thanks, back to work!")
 
-        return f'/mnt/c/users/{win_user}/.ssh'
+        return f"/mnt/c/users/{win_user}/.ssh"
     else:
-        return f'/home/{os.getlogin()}/.ssh'
-    
+        return f"/home/{os.getlogin()}/.ssh"
+
+
 def make_config(entries: list[SSHConfigEntry], path: str) -> None:
     """Generate the SSH config file itself
 
@@ -163,84 +173,120 @@ def make_config(entries: list[SSHConfigEntry], path: str) -> None:
     # Create a backup of the old directory if it already exists
     if os.path.exists(path):
         import time
-        new_loc = shutil.make_archive(base_name=f'{path}-{time.strftime("%Y%m%d-%H%M%S")}.old', format='gztar', root_dir=path)
-        print(f'Moving old version of .ssh directory to a backup location at "{new_loc}"')
+
+        new_loc = shutil.make_archive(
+            base_name=f'{path}-{time.strftime("%Y%m%d-%H%M%S")}.old',
+            format="gztar",
+            root_dir=path,
+        )
+        print(
+            f'Moving old version of .ssh directory to a backup location at "{new_loc}"'
+        )
         shutil.rmtree(path)
-    
+
     os.mkdir(path)
-    
-    with open(os.path.join(path, 'config'), 'w') as fout:
-        fout.write('# This file was automatically generated by a Chezmoi script. Changes will be overwritten if `chezmoi apply` or `chezmoi update` is ran.\n')
-        fout.write(f'# You can find the script somewhere in the directory opened with `chezmoi cd`.\n\n')
+
+    with open(os.path.join(path, "config"), "w") as fout:
+        fout.write(
+            "# This file was automatically generated by a Chezmoi script. Changes will be overwritten if `chezmoi apply` or `chezmoi update` is ran.\n"
+        )
+        fout.write(
+            "# You can find the script somewhere in the directory opened with `chezmoi cd`.\n\n"
+        )
 
         if not is_wsl():
             # Specify the 1Password agent if we aren't in WSL. Windows uses a global pipe instead
-            fout.write('Host *\n\tIdentityAgent ~/.1password/agent.sock\n')
+            fout.write("Host *\n\tIdentityAgent ~/.1password/agent.sock\n")
         for entry in entries:
-            fout.write('\n')
+            fout.write("\n")
             fout.write(entry.serialize(path))
-    
-    os.chmod(path, 0o700)
-    os.chmod(os.path.join(path, 'config'), 0o600)
 
-def question(q: str, default: str = 'y') -> bool:
+    os.chmod(path, 0o700)
+    os.chmod(os.path.join(path, "config"), 0o600)
+
+
+def question(q: str, default: str = "y") -> bool:
     res = input(q).lower()
     if not res:
         res = default
 
     match res:
-        case 'y':
+        case "y":
             return True
-        case 'n':
+        case "n":
             return False
         case _:
             return question(q, default=default)
 
+
 def main():
+    if not question("Generate an SSH directory from 1Password? (Y/n) "):
+        sys.exit(0)
+
     # Identify the 1Password CLI and quit if it's not installed
     OP_EXE = get_op_exe()
     if OP_EXE is None:
-        print("1Password CLI not installed! Quitting.", file=sys.stderr)
+        print(
+            "1Password CLI not installed! You can rerun this script with `chezmoi update`. Quitting.",
+            file=sys.stderr,
+        )
         if is_wsl():
-            print("Hint: If you're trying to use WSL, you need the Windows CLI installed, not Linux!", file=sys.stderr)
-        sys.exit(1)
+            print(
+                "Hint: If you're trying to use WSL, you need the Windows CLI installed, not Linux!",
+                file=sys.stderr,
+            )
+        sys.exit(0)
 
-    print('Generating a new SSH directory...')
+    print("Generating a new SSH directory...")
 
     # Create a backup of the old directory if applicable!
     known_hosts = None
     path = get_ssh_path()
     if os.path.exists(path):
-        if not question('This will replace your existing directory, but a backup will be made. Continue? (Y/n) '):
+        if not question(
+            "This will replace your existing directory, but a backup will be made. Continue? (Y/n) "
+        ):
             sys.exit(0)
-        if question('Preserve known_hosts file? Recommended to answer "n" if you don\'t know what this is. (Y/n) '):
-            with open(os.path.join(path, 'known_hosts'), 'r') as fin:
-                known_hosts = fin.read()
-    
+        known_hosts_path = os.path.join(path, "known_hosts")
+        if os.path.exists(known_hosts_path):
+            if question(
+                'Preserve known_hosts file? Recommended to answer "n" if you don\'t know what this is. (Y/n) '
+            ):
+                with open(known_hosts_path, "r") as fin:
+                    known_hosts = fin.read()
+
     # Get all SSH Key entries by ID
-    get_entries_res = call_shell(f'{OP_EXE} item list --categories SSHKEY --format json')
+    get_entries_res = call_shell(
+        f"{OP_EXE} item list --categories SSHKEY --format json"
+    )
     key_ids = []
     for key in json.loads(get_entries_res):
         # Omit the signing key - this is hardcoded! :(
-        if re.search('signing', key['title'], re.IGNORECASE):
-            if not question(f'Key "{key['title']}" appears to be for signing. Load anyways? (y/N) ', default='n'):
+        if re.search("signing", key["title"], re.IGNORECASE):
+            if not question(
+                f'Key "{key['title']}" appears to be for signing. Load anyways? (y/N) ',
+                default="n",
+            ):
                 continue
 
-        key_ids.append(key['id'])
+        key_ids.append(key["id"])
 
     # Get desired fields from each entry by entry ID
     entries: list[SSHConfigEntry] = []
     for key in key_ids:
-        res = call_shell(f'{OP_EXE} item get --format json --fields label="public key",label="chezmoi params" {key}')
+        res = call_shell(
+            f'{OP_EXE} item get --format json --fields label="public key",label="chezmoi params" {key}'
+        )
         entry = SSHConfigEntry(json.loads(res))
         entries.append(entry)
 
     make_config(entries, path)
     # Restore the known_hosts file if desired
     if known_hosts:
-        with open(os.path.join(path, 'known_hosts'), 'w') as fout:
+        with open(os.path.join(path, "known_hosts"), "w") as fout:
             fout.write(known_hosts)
-        os.chmod(os.path.join(path, 'known_hosts'), 0o600)
+        os.chmod(os.path.join(path, "known_hosts"), 0o600)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
