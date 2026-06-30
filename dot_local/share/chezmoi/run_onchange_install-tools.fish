81d0c0bea9ba001c0209494572beeebe6aa6d1ef
Imani Pelton <imani.pelton@canonical.com>
2026-06-30 14:46:57 -0400

    simplify emit event

--- dot_local/share/chezmoi/run_onchange_install-tools.fish
+++ dot_local/share/chezmoi/run_onchange_install-tools.fish
@@ -60,7 +60,7 @@ end
 set -l _tide_tmp_dir (command mktemp -d)
 curl https://codeload.github.com/ilancosman/tide/tar.gz/v6 | tar -xzC $_tide_tmp_dir
 command cp -R $_tide_tmp_dir/*/{completions,conf.d,functions} $__fish_config_dir
-fish_path=(status fish-path) exec $fish_path -C "emit _tide_init_install"
+fish -C "emit _tide_init_install"
 set -e _tide_tmp_dir
 
 # Configure fish plugins
