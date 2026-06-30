5ca7544a201404c5339aec1315b70684a5d499fb
Imani Pelton <imani.pelton@canonical.com>
2026-06-30 19:36:30 -0400

    back up more color configs

---
+++ dot_config/private_fish/conf.d/fish_colors.fish
@@ -0,0 +1,28 @@
+set -U fish_color_autosuggestion brblack
+set -U fish_color_cancel -r
+set -U fish_color_command blue
+set -U fish_color_comment red
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end green
+set -U fish_color_error brred
+set -U fish_color_escape brcyan
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_normal normal
+set -U fish_color_operator brcyan
+set -U fish_color_param cyan
+set -U fish_color_quote yellow
+set -U fish_color_redirection 'cyan'  '--bold'
+set -U fish_color_search_match 'white'  '--background=brblack'
+set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_key_bindings fish_default_key_bindings
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description 'yellow'  '-i'
+set -U fish_pager_color_prefix 'normal'  '--bold'  '--underline'
+set -U fish_pager_color_progress 'brwhite'  '--background=cyan'
+set -U fish_pager_color_selected_background -r
