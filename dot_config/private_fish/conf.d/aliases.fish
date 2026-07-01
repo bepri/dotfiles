d10d69fb770ea38dd56817699db47d749d0b0fc5
Imani Pelton <imani.pelton@canonical.com>
2026-06-30 20:02:37 -0400

    touchups

--- dot_config/private_fish/conf.d/aliases.fish
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -1,2 +1,7 @@
-alias cat $HOME/.cargo/bin/bat
+if file -f $HOME/.cargo/bin/bat
+    alias cat $HOME/.cargo/bin/bat
+else if command -q batcat
+    alias cat $(which batcat)
+end
+
 alias fd /usr/bin/fdfind
--- dot_config/private_fish/conf.d/tide_config.fish
+++ dot_config/private_fish/conf.d/tide_config.fish
@@ -1,1 +1,2 @@
 set -U tide_left_prompt_items 'os'  'pwd'  'git_custom'  'newline'  'character'
+set -U tide_right_prompt_items 'status'  'cmd_duration'  'context'  'jobs'  'direnv'  'bun'  'node'  'python_custom'  'rustc'  'java'  'php'  'pulumi'  'ruby'  'go'  'kubectl'  'distrobox'  'toolbox'  'terraform'  'aws'  'nix_shell'  'crystal'  'elixir'  'zig'  'time'
